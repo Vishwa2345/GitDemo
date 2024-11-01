8348f83d100f98e05571e9e26e5f7bf64b9ab630
vishwa2345 <babluvish@gmail.com>
2024-11-01 15:42:19 +0530

    adding new class name student

---
+++ GitDemo/src/com/gitpackage/Student.java
@@ -0,0 +1,20 @@
+package com.gitpackage;
+
+public class Student {
+	
+	private String nameString;
+	private String lastNameString;
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	public String getLastNameString() {
+		return lastNameString;
+	}
+	public void setLastNameString(String lastNameString) {
+		this.lastNameString = lastNameString;
+	}
+
+}
